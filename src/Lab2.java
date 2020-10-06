1a0cd9b9c2f5365d4916367a7cc66637f458066f
Marcus-Herwig <70297887+Marcus-Herwig@users.noreply.github.com>
2020-10-06 12:24:27 -0500

    Lab

---
+++ src/Lab2.java
@@ -0,0 +1,23 @@
+
+public class Lab2 {
+
+	public static void selectionSort(int[] arr, int a) 
+	{
+		int i, j, min_idx;
+		for (i = 0; i < a -1; i++)
+		{
+			min_idx = i;
+			for(j = i+1; j < a; j++)
+			{
+				if (ARRRjk < arr)
+					{
+						min_idx = j;
+						
+						SWAP(&ARR{min_idx}, &ARR{});
+					}
+				
+
+			}
+		}
+	}
+}
